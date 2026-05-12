b4c3ff16e89d8a1a47b44122584aea2eb2500798
Peyton Markus <150070019+pmark2047@users.noreply.github.com>
2026-05-12 14:23:09 -0600

    Added files via upload

---
+++ testNode.cpp
@@ -0,0 +1,32 @@
+/***********************************************************************
+ * Header:
+ *    Test
+ * Summary:
+ *    Driver to test node.h
+ * Author
+ *    Br. Helfrich
+ ************************************************************************/
+
+#ifndef DEBUG
+#define DEBUG   // Remove this to skip the unit tests
+#endif // DEBUG
+
+#include "testSpy.h"        // for the spy unit tests
+#include "testNode.h"       // for the unit tests
+int Spy::counters[] = {};
+
+/**********************************************************************
+ * MAIN
+ * This is just a simple menu to launch a collection of tests
+ ***********************************************************************/
+int main()
+{
+#ifdef DEBUG
+   // unit tests
+   TestSpy().run();
+   TestNode().run();
+#endif // DEBUG
+  
+   return 0;
+}
+
